4d76d68040ea467baf99ec02b0693c8caaf9b703
WillyLu326 <luzh0326@gmail.com>
2017-11-13 22:13:36 -0500

    create initialState

--- redux-basic.js
+++ redux-basic.js
@@ -1,12 +1,18 @@
 const redux = require('redux');
 const createStore = redux.createStore;
 
+const initialState = {
+  counter: 0
+};
+
 // Reducer
-const rootReducer = (state, action) => {
+const rootReducer = (state = initialState , action) => {
   return state;
 }
 
 // Store
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 // Dispatching Action
 
