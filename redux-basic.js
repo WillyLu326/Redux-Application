23befc7c83ed9cad47de687e08efbb213b6547cc
WillyLu326 <luzh0326@gmail.com>
2017-11-13 22:15:59 -0500

    add dispatching action

--- redux-basic.js
+++ redux-basic.js
@@ -15,5 +15,7 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 // Dispatching Action
+store.dispatch({ type: 'INC_COUNTER' });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
 
 // Subscription
