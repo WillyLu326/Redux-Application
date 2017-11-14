ea26b0ba7cd038dfe39edc147c4d276f034bcd8b
WillyLu326 <luzh0326@gmail.com>
2017-11-13 22:40:48 -0500

    import Provider from  react-redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import Reducer from './../src/store/reducer';
+import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
@@ -9,5 +10,5 @@ import registerServiceWorker from './registerServiceWorker';
 
 const store = createStore(Reducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
