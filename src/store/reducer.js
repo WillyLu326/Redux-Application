707bc6bd9fef0e932078ad2fd18bac0bcb9f77b9
WillyLu326 <luzh0326@gmail.com>
2017-11-14 20:47:32 -0500

    add switch into reducer.js

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,19 @@
+import Type from './../Type';
+
 const initialState = {
   counter: 100
 }
 
 const Reducer = (state = initialState, action) => {
-  return state;
+  switch (action.type) {
+    case Type.INCREMENT:
+      return {
+        ...state,
+        counter: state.counter + 1
+      }
+    default:
+      return state;
+  }
 }
 
 export default Reducer;
